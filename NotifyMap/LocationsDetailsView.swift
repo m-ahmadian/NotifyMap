5569bef9bff9a4bc4903e88d05b018ad7dff853d
m-ahmadian <mehrdad.ahmadian@gmail.com>
2024-01-08 14:04:39 -0500

    Add LocationsDetailsView

---
+++ NotifyMap/LocationsDetailsView.swift
@@ -0,0 +1,112 @@
+//
+//  LocationsDetailsView.swift
+//  NotifyMap
+//
+//  Created by Mehrdad Behrouz Ahmadian on 2024-01-08.
+//
+
+import SwiftUI
+import MapKit
+
+struct LocationsDetailsView: View {
+    @Binding var mapSelection: MKMapItem?
+    @Binding var show: Bool
+    @State private var loockAroundScene: MKLookAroundScene?
+    @Binding var getDirections: Bool
+    
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text(mapSelection?.placemark.name ?? "")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .padding()
+                    
+                    Text(mapSelection?.placemark.title ?? "")
+                        .font(.footnote)
+                        .foregroundStyle(.gray)
+                        .lineLimit(2)
+                        .padding(.trailing)
+                }
+                
+                Spacer()
+                
+                Button {
+                    show.toggle()
+                    mapSelection = nil
+                } label: {
+                    Image(systemName: "xmark.circle.fill")
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                        .foregroundStyle(.gray, Color(.systemGray6))
+                }
+
+            }
+            
+            if let scene = loockAroundScene {
+                LookAroundPreview(initialScene: scene)
+                    .frame(height: 200)
+                    .cornerRadius(12)
+                    .padding()
+            } else {
+                ContentUnavailableView("No preview available", systemImage: "eye.slash")
+            }
+            
+            HStack(spacing: 24) {
+                Button {
+                    if let mapSelection {
+                        mapSelection.openInMaps()
+                    }
+                } label: {
+                    Text("Open in Maps")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(width: 170, height: 48)
+                        .background(.green)
+                        .cornerRadius(12)
+                }
+                
+                Button {
+                    getDirections = true
+                    show = false
+                } label: {
+                    Text("Get Directions")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(width: 170, height: 48)
+                        .background(.blue)
+                        .cornerRadius(12)
+                }
+
+
+            }
+            
+        }
+        .onAppear {
+            print("DEBUG: Did call on appear")
+            fetchLookAroundPreview()
+        }
+        .onChange(of: mapSelection) { oldValue, newValue in
+            print("DEBUG: Did call on change")
+            fetchLookAroundPreview()
+        }
+        .padding()
+    }
+}
+
+extension LocationsDetailsView {
+    func fetchLookAroundPreview() {
+        if let mapSelection {
+            loockAroundScene = nil
+            Task {
+                let request = MKLookAroundSceneRequest(mapItem: mapSelection)
+                loockAroundScene = try? await request.scene
+            }
+        }
+    }
+}
+
+#Preview {
+    LocationsDetailsView(mapSelection: .constant(nil), show: .constant(false), getDirections: .constant(false))
+}
