28f60391088efe959c870188ea859267da16ca04
m-ahmadian <mehrdad.ahmadian@gmail.com>
2024-01-08 13:56:25 -0500

    Add Xcode project

---
+++ NotifyMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotifyMap
+//
+//  Created by Mehrdad Behrouz Ahmadian on 2024-01-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotifyMap/NotifyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotifyMapApp.swift
+//  NotifyMap
+//
+//  Created by Mehrdad Behrouz Ahmadian on 2024-01-08.
+//
+
+import SwiftUI
+
+@main
+struct NotifyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
